6f3d24d19b25ed4138d50d5c333b90105a1927b2
brianlball <brian.ball@nrel.gov>
2024-05-01 10:04:55 -0400

    appveyor ruby 3.2

--- ci/appveyor/setup.cmd
+++ ci/appveyor/setup.cmd
@@ -1,6 +1,6 @@
 REM set PATH=C:\projects\ruby\bin;C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
-set PATH=C:\Ruby27-x64\bin;C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
-set BUNDLE_VERSION=2.1.4
+set PATH=C:\Ruby32-x64\bin;C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
+set BUNDLE_VERSION=2.4.10
 set GEM_HOME=C:\projects\openstudio-server\gems
 set GEM_PATH=C:\projects\openstudio-server\gems;C:\projects\openstudio-server\gems\gems\bundler\gems
 
@@ -10,7 +10,7 @@ set OS_INSTALL_NAME=OpenStudio-%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%%%2B%
 echo Install name is %OS_INSTALL_NAME%
 
 REM curl -SLO --insecure https://openstudio-ci-builds.s3-us-west-2.amazonaws.com/develop/%OS_INSTALL_NAME%
-curl -SLO --insecure  https://github.com/NREL/OpenStudio/releases/download/v3.7.0/%OS_INSTALL_NAME%
+curl -SLO --insecure  https://github.com/NREL/OpenStudio/releases/download/v3.8.0/%OS_INSTALL_NAME%
 dir .
 REM Install OpenStudio
 %OS_INSTALL_NAME% --script ci/appveyor/install-windows.qs
