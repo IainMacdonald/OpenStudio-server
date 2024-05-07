21465c91d607f1c6901a8c097d0d531e02218e4f
brianlball <brian.ball@nrel.gov>
2024-05-07 10:23:42 -0400

    3.8.0-rc2

--- server/app/lib/openstudio_server/version.rb
+++ server/app/lib/openstudio_server/version.rb
@@ -7,6 +7,6 @@ module OpenstudioServer
   VERSION = '3.8.0'.freeze
   # format should be ^.*\-{1}[a-z]+[0-9]+
   # for example: -rc1, -beta6, -customusecase0
-  VERSION_EXT = '-rc1'.freeze # with preceding - or +
-  OS_SHA = '2d5664af4d'.freeze
+  VERSION_EXT = '-rc2'.freeze # with preceding - or +
+  OS_SHA = 'af997f4fef'.freeze
 end
