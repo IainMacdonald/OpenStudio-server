d3d37b2f6900f9344fc08338059c6862115a0e77
brianlball <brian.ball@nrel.gov>
2024-05-08 19:01:56 -0400

    try installing devtools

--- ci/appveyor/setup.cmd
+++ ci/appveyor/setup.cmd
@@ -1,38 +1,72 @@
-REM set PATH=C:\projects\ruby\bin;C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
+@echo off
+REM Set initial PATH with Git and Ruby binaries
 set PATH=C:\Ruby32-x64\bin;C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
+
+REM Set Bundler version
 set BUNDLE_VERSION=2.4.10
+
+REM Configure GEM paths
 set GEM_HOME=C:\projects\openstudio-server\gems
-set GEM_PATH=C:\projects\openstudio-server\gems;C:\projects\openstudio-server\gems\gems\bundler\gems
+set GEM_PATH=C:\projects\openstudio-server\gems;C:\projects\openstudio-server\gems\bundler\gems
 
 echo Downloading and Installing OpenStudio (develop branch, %OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%+%OPENSTUDIO_VERSION_SHA%)
-REM install develop build
 set OS_INSTALL_NAME=OpenStudio-%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%+%OPENSTUDIO_VERSION_SHA%-Windows.exe
-
 echo Install name is %OS_INSTALL_NAME%
 
-REM curl -SLO --insecure https://openstudio-ci-builds.s3-us-west-2.amazonaws.com/develop/%OS_INSTALL_NAME%
-REM curl -SLO --insecure  https://github.com/NREL/OpenStudio/releases/download/v3.8.0/%OS_INSTALL_NAME%
-curl -SLO --insecure  https://github.com/NREL/OpenStudio/releases/download/v%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%/%OS_INSTALL_NAME%
+REM Download and Install OpenStudio
+curl -SLO --insecure https://github.com/NREL/OpenStudio/releases/download/v%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%/%OS_INSTALL_NAME%
 dir .
-REM Install OpenStudio
+
+REM Execute the OpenStudio installer
 %OS_INSTALL_NAME% --script ci/appveyor/install-windows.qs
 move C:\openstudio C:\projects\openstudio
 dir C:\projects\openstudio
-REM dir C:\projects\openstudio\Ruby
-rm %OS_INSTALL_NAME%
+
+REM Cleanup installer
+del %OS_INSTALL_NAME%
+
+REM Show Ruby version
 ruby -v
 openstudio openstudio_version
 
-REM If you change RUBYLIB here, make sure to change it in integration-test.ps1 and unit-test.cmd too
+REM Setup MSYS2 and MinGW toolchain
+echo Setting up MSYS2 and MinGW toolchain
+call ridk install 2 3
+
+REM Uninstall any existing Bundler
+echo Uninstalling existing versions of Bundler
+call gem uninstall -aIx bundler
+
+REM Install specified version of Bundler
+echo Installing Bundler %BUNDLE_VERSION%
+call gem install bundler -v %BUNDLE_VERSION%
+if %ERRORLEVEL% neq 0 (
+    echo Failed to install Bundler %BUNDLE_VERSION%
+    exit /b %ERRORLEVEL%
+)
+
+REM Verify Bundler installation
+call bundle --version
+if %ERRORLEVEL% neq 0 (
+    echo Bundler was not installed correctly.
+    exit /b %ERRORLEVEL%
+)
+
+REM Set RUBYLIB environment variable
 set RUBYLIB=C:\projects\openstudio\Ruby
 
-call gem install bundler -v %BUNDLE_VERSION%
-which bundle
-call bundle --version
+REM Install gems as specified
+echo Installing required Ruby gems...
+call bundle install --verbose
+if %ERRORLEVEL% neq 0 (
+    echo Gem installation failed.
+    exit /b %ERRORLEVEL%
+)
 
-ruby C:\projects\openstudio-server\bin\openstudio_meta install_gems --with_test_develop --debug --verbose
-REM dying over next 2 lines w/ "system cannot find path specified" - maybe just ruby.exe?
+REM Navigate to the server directory and run the gem installation script
 cd c:\projects\openstudio-server
-bundle install
-REM echo List out the test Directory
-REM dir C:\projects\openstudio-server\spec\files\
+call ruby C:\projects\openstudio-server\bin\openstudio_meta install_gems --with_test_develop --debug --verbose
+if %ERRORLEVEL% neq 0 (
+    echo Gem installation script failed.
+    exit /b %ERRORLEVEL%
+)
